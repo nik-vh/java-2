cfc94234c49b7d24a7e5f8e010385f649e56f492
nik-vh <nikowsun@gmail.com>
2020-08-14 00:22:22 +0300

    Lesson5

---
+++ lesson5/src/main/java/lesson5/Main.java
@@ -0,0 +1,11 @@
+package lesson5;
+
+public class Main {
+    public static void main(String... arg) {
+
+        ChangArray changArray = new ChangArray();
+        changArray.method1();
+        changArray.method2();
+    }
+
+}
